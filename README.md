014e0cfbe426c76b73bef6d877843fc84664407e
idgleb2 <id_gleb@mail.ru>
2024-09-11 04:09:23 -0300

    3

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # VTV
+
+
  
+![Diagrama en blanco.png](Diagrama%20en%20blanco.png)
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+
+        VTV vtv = null;
+
+        int seleccion = 0;
+        do {
+            String[] opciones = {"revisarVehiculo", "getReporte", "Salir"};
+            seleccion = JOptionPane.showOptionDialog(
+                    null,
+                    "Seleccione una opción:",
+                    "Ursol",
+                    JOptionPane.DEFAULT_OPTION,
+                    JOptionPane.INFORMATION_MESSAGE,
+                    null,
+                    opciones,
+                    opciones[0]
+            );
+            switch (seleccion) {
+                case 0:
+                    String patente = JOptionPane.showInputDialog("Ingrese un patente");
+                    vtv = new VTV(patente);
+                    vtv.revisarVehiculo();
+                    break;
+                case 1:
+                    if (vtv!=null){
+                        vtv.getReporte();
+                    }else JOptionPane.showMessageDialog(null, "No hay vehiculos, de primero pulsa <revisar>");
+                    break;
+                case 2:
+                    break;
+                default:
+                    break;
+            }
+        }while (seleccion!=2);
+
 
     }
 }
